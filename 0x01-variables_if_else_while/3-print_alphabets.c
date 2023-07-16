f4d49831965dfaa91807a6417854848134758125
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-16 14:01:37 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,21 +6,23 @@
  * and then in uppercase, followed by a new line.
  * Return: 0
  */
-int main() {
+int main(void) 
+{
     char l = 'a';
     char L = 'A';
 
-    while (l <= 'z') {
+    while (l <= 'z') 
+    {
         putchar(l);
         l++;
     }
 
-    while (L <= 'Z') {
+    while (L <= 'Z') 
+    {
         putchar(L);
         L++;
     }
 
     putchar('\n');
-
-    return 0;
+    return (0);
 }
