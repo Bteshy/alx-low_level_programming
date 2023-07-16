09ef9438d906f2cbc17d7f69e19fc14f5f250bc1
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-16 13:44:33 -0700

    Update 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stidio.h>
+
+/**
+ * main - main block
+ * Description:  prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: 0
+ */
+int main() {
+    char l = 'a';
+    char L = 'A';
+
+    // Print lowercase alphabet
+    while (l <= 'z') {
+        putchar(l);
+        l++;
+    }
+
+    // Print uppercase alphabet
+    while (L <= 'Z') {
+        putchar(L);
+        L++;
+    }
+
+    // Print new line
+    putchar('\n');
+
+    return 0;
+}
