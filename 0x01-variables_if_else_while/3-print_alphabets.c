9ebc3174003602fbbf08427d36789f9372d4c86e
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-16 13:52:04 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,19 +10,16 @@ int main() {
     char l = 'a';
     char L = 'A';
 
-    // Print lowercase alphabet
     while (l <= 'z') {
         putchar(l);
         l++;
     }
 
-    // Print uppercase alphabet
     while (L <= 'Z') {
         putchar(L);
         L++;
     }
 
-    // Print new line
     putchar('\n');
 
     return 0;
