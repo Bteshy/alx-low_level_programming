00fd3ad16ce4a61be93552d5ef6b77dcb6d5b4fc
Bteshy <brendachepngetich123@gmail.com>
2023-07-19 12:50:10 -0700

    done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int limit = 4000000;
-    int fib1 = 1, fib2 = 2, fib = 0;
-    int sum = 0;
+int main(void)
+{
+	int limit = 4000000;
+	int fib1 = 1, fib2 = 2, fib = 0;
+	int sum = 0;
 
-    while (fib1 <= limit) {
-        if (fib1 % 2 == 0) {
-            sum += fib1;
-        }
-        fib = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = fib;
-    }
+	while (fib1 <= limit)
+	{
+		if (fib1 % 2 == 0)
+		{
+			sum += fib1;
+		}
 
-    printf("%d\n", sum);
-
-    return 0;
+		fib = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
