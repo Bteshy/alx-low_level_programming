79f540b56be8132a4f443b19bdb97d62ed4a3792
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-19 10:54:27 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,8 @@
 
 void print_times_table(int n)
 {
-	int row, column, product, tens, ones, hundreds;
+	int row, column, product;
+
 	if (n >= 0 && n < 15)
 	{
 		for (row = 0; row <= n; row++)
@@ -19,9 +20,7 @@ void print_times_table(int n)
 			for (column = 0; column <= n; column++)
 			{
 				product = row * column;
-				hundreds = product / 100;
-				tens = (product / 10) % 10;
-				ones = product % 10;
+
 				if (column == 0)
 					_putchar('0');
 				else if (product < 10)
@@ -30,23 +29,23 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(ones + '0');
+					_putchar(product % 10 + '0');
 				}
 				else if (product >= 10 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(tens + '0');
-					_putchar(ones + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
 				}
-				else if (product >= 100 && product < 1000)
+				else if (product > 99 && product < 1000)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(hundreds + '0');
-					_putchar(tens + '0');
-					_putchar(ones + '0');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
 				}
 			}
 			_putchar('\n');
