3faa0ef3707c2f21ad859314acd188cac7b4a715
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-20 06:10:23 -0700

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+
 void positive_or_negative(int i)
 {
 	if (i < 0)
