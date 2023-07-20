334fb38e5f2eeb3b08609c777a84d9d0c3924a73
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-20 06:38:28 -0700

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,13 +2,10 @@
 #include "main.h"
 
 /**
- * positive_or_negative - Determines if a given integer is positive, negative, or zero.
+ * positive_or_negative - Determines if an integer is positive, negative, or zero.
  * @i: The integer to be evaluated.
  *
- * This function takes an integer as input and prints whether it is positive, negative, or zero.
- * If the integer is less than 0, it prints "<i> is negative".
- * If the integer is equal to 0, it prints "0 is zero".
- * If the integer is greater than 0, it prints "<i> is positive".
+ * Description: Prints whether the integer is positive, negative, or zero.
  */
 
 void positive_or_negative(int i)
