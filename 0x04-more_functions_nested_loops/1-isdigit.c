f006b12bfb19424bf1ba4a9096c9405654ffe1be
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-21 06:35:48 -0700

    Update 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - checks if value is a digit
+ * @c: value to be checked
+ * Return: 1 if true, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int res;
+
+	if (isdigit(c))
+		respo = 1;
+	else
+		respo = 0;
+
+	return (respo);
+}
