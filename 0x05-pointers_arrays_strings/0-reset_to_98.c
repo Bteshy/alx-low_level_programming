ffe29d536116196b14b496709987da8f3103fd5d
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-24 08:07:30 -0700

    Update 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *reset_to_98 - updates the value it points to to 98
+  *@n- value
+  *return 0
+  *
+  */
+
+void reset_to_98(int *n)
+ int n;
+int *p;
+
+n = 98;
+p= &n;
+
+return (0);
