a14c3d1e1423ec1d5264a8179f8176b1c82fa27d
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-25 14:23:22 -0700

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ *
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+  int len = strlen(s);
+    int start = 0;
+    int end = length - 1;
+    
+    while (start < end) 
+    {
+      char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+
+    start++;
+        end--;
+    }
+
+}
