16f179157eb43cfde898fec7228fcf0a1ad9377a
Bteshy <brendachepngetich123@gmail.com>
2023-07-25 13:18:48 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,12 @@
  */
 void print_rev(char *s)
 {
-  int len = strlen(s);
-  int i = 0;
+	int len = strlen(s);
+	int i = 0;
 
-  for (i = len - 1; i >= 0; i--)
-    {
-        printf("%c", s[i]);
-    }
-  printf("\n");
+	for (i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
 }
