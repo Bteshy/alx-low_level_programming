b27b7df5a04bce284559b3d9d833a6be378c30ae
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-07-25 13:01:06 -0700

    Update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints string backwards
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+  int len = strlen(s);
+
+  for (int i = len - 1; i >= 0; i--)
+    {
+        printf("%c", s[i]);
+    }
+  printf("\n");
+}
