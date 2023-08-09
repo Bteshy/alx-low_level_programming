5a46926fc02e38f6491ce0ef884e8f9a368770b6
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-08-09 13:34:33 -0700

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,36 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
 
-char *argstostr(int ac, char **av) {
-    if (ac == 0 || av == NULL) {
+int is_space(char c) {
+    return (c == ' ' || c == '\t' || c == '\n');
+}
+
+char **strtow(char *str) {
+    if (str == NULL || *str == '\0') {
         return NULL;
     }
 
-    // Calculate the total length needed for the concatenated string
-    int total_length = 0, int i = 0;
-    for (i = 0; i < ac; i++) {
-        total_length += strlen(av[i]) + 1; // +1 for newline character
+    int num_words = 0;
+    char **words = NULL;
+
+    char *c = str;
+    while (*c != '\0') {
+        while (is_space(*c)) {
+            c++;
+        }
+
+        if (*c == '\0') {
+            break;
+        }
+
+        num_words++;
+
+        char *word_start = c;
+        while (*c && !is_space(*c)) {
+            c++;
+        }
+
+        char *word = (char *)malloc((c - word_start + 1) * sizeof(char));
+        if (word == NULL) {
+            for (int i = 0; i < num_words - 1; i++) {
+                free(words[i]);
+            }
+            free(words);
+            return NULL;
+        }
+
+        strncpy(word, word_start, c - word_start);
+        word[c - word_start] = '\0';
+
+        words = (char **)realloc(words, num_words * sizeof(char *));
+        if (words == NULL) {
+            free(word);
+            for (int i = 0; i < num_words - 1; i++) {
+                free(words[i]);
+            }
+            free(words);
+            return NULL;
+        }
+
+        words[num_words - 1] = word;
     }
 
-    // Allocate memory for the concatenated string
-    char *result = (char *)malloc(total_length * sizeof(char));
-    if (result == NULL) {
+    words = (char **)realloc(words, (num_words + 1) * sizeof(char *));
+    if (words == NULL) {
+        for (int i = 0; i < num_words; i++) {
+            free(words[i]);
+        }
+        free(words);
         return NULL;
     }
 
-    int current_index = 0;
+    words[num_words] = NULL;
 
-    // Copy each argument with newline into the concatenated string
-    for (i = 0; i < ac; i++) {
-        strcpy(result + current_index, av[i]);
-        current_index += strlen(av[i]);
-        result[current_index] = '\n';
-        current_index++;
-    }
-
-    result[current_index] = '\0'; // Add the null-terminator
-
-    return result;
+    return words;
 }
