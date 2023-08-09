04ad4fdade4c39d55993dcba06a2715b086afa74
Bteshy <brendachepngetich123@gmail.com>
2023-08-09 03:18:07 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,11 @@
  * Return: 0 if successful
  */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	(void) argc;
 
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
 
