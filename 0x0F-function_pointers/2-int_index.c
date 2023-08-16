2c75996a463206768bd2821a8906fc1bd06280a2
Brenda rotich <93199080+Bteshy@users.noreply.github.com>
2023-08-16 02:37:11 -0700

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,23 +8,23 @@
   *
   * Return: ...
   */
-
-int int_index(int *array, int size, int (*cmp)(int)) 
+int int_index(int *array, int size, int (*cmp)(int))
 {
-  int i = 0;
-  
-    if (size <= 0) 
-    {
-        return -1;
-    }
+	int i = 0;
 
-    for (i = 0; i < size; i++)
-      {
-        if (cmp(array[i]) != 0) 
-        {
-            return i;
-        }
-    }
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
 
-    return -1;
+				i++;
+			}
+		}
+	}
+
+	return (-1);
 }
